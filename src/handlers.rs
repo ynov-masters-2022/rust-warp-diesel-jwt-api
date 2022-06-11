aea9326c6c15ce01d2d58315d9beb1a9b52b9548
Lucas <lucas@ld-web.net>
2022-06-11 16:25:58 +0200

    :ok_hand: update users list response format
    Unwrapped Result

--- src/handlers.rs
+++ src/handlers.rs
@@ -3,7 +3,7 @@ use std::convert::Infallible;
 use crate::{models::NewUser, users};
 
 pub async fn list_users() -> Result<impl warp::Reply, Infallible> {
-  Ok(warp::reply::json(&users::get_all()))
+  Ok(warp::reply::json(&users::get_all().unwrap()))
 }
 
 pub async fn create_user(new_user: NewUser) -> Result<impl warp::Reply, Infallible> {
