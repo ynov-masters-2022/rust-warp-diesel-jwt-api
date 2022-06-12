12e76e3b2318d9e2a26f7bf6b3ac059ec81b4900
Lucas <lucas@ld-web.net>
2022-06-12 14:05:25 +0200

    :sparkles: add error handling in main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use warp_diesel_jwt_api::routes;
+use warp::Filter;
+use warp_diesel_jwt_api::{error, routes};
 
 #[tokio::main]
 async fn main() {
-  let users_routes = routes::users_routes();
+  let routes = routes::routes().recover(error::handle_rejection);
 
-  warp::serve(users_routes).run(([127, 0, 0, 1], 3500)).await;
+  warp::serve(routes).run(([127, 0, 0, 1], 3500)).await;
 }
